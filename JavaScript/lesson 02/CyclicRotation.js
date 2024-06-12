5aed9e30df8ebfd768d08995303eae32120ba777
RedHill <155854128+redhill13@users.noreply.github.com>
2024-06-12 23:09:11 +0200

    removed non python code

--- JavaScript/lesson 02/CyclicRotation.js
+++
@@ -1,8 +0,0 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
-function solution(A, K) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    K = K % A.length;
-    return A.slice(A.length - K).concat(A.slice(0, A.length - K));
-}
